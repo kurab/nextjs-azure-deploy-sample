d8de091833f871a75c4b8e52744045afdf1138f9
Hiroshi Kurabayashi <kura@indi-solution.co.jp>
2022-01-31 12:08:28 +0700

    add mathrand api

---
+++ pages/api/mathrand.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  number: number
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const mathrand = Math.random();
+  res.status(200).json({ number: mathrand })
+}
